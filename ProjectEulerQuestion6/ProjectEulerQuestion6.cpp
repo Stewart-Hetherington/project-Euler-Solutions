03ea2cd48fb585d762c6cec0e75ccc9d6af83005
Stewart Hetherington <stewart.hetherington.2016@mumail.ie>
2018-08-04 12:37:30 +0100

    Create ProjectEulerQuestion6.cpp
    Translate my project Euler Q6 java file into a new C++ file.

---
+++ ProjectEulerQuestion6/ProjectEulerQuestion6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+/*The sum of the squares of the first ten natural numbers is,
+ *12 + 22 + ... + 102 = 385
+ *The square of the sum of the first ten natural numbers is,
+ *(1 + 2 + ... + 10)2 = 552 = 3025
+ *Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+ *Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ *
+ *Author: Stewart Hetheringnton.
+ **/
+int main()
+{
+    int sumOfSquare=0;
+    int sum=0;
+    for(int i=1; i<=100; i++) {
+        sum += i;
+        sumOfSquare += i*i;
+    }
+    int squareOfSum = sum*sum;
+    printf("%d", squareOfSum-sumOfSquare);
+    return 0;
+}
